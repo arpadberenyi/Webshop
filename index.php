f7e5970cf608e6b76ecdef801e77053066c02851
arpadberenyi <vendetta21@gmail.com>
2021-01-31 10:36:24 +0100

    second commit

--- index.php
+++ index.php
@@ -19,10 +19,6 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <script src='https://kit.fontawesome.com/a076d05399.js'></script>
  
-
-
-
-
     <title>Webshop</title>
 
 </head>
